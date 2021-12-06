1125f1d9b9c14aaaa121e587de5b8b7fc60777db
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2021-12-05 23:48:42 -0300

    chore: updating jsx

--- App.js
+++ App.js
@@ -80,9 +80,7 @@ const App: () => Node = () => {
           <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
+          <Section title="Learn More">Let me test this</Section>
           <LearnMoreLinks />
         </View>
       </ScrollView>
